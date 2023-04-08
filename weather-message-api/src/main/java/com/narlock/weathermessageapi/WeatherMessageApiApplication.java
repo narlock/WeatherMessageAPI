f9464d9121e33b4a1b414d709fd96375a470a364
Anthony <59890724+narlock@users.noreply.github.com>
2023-04-08 07:10:38 -0500

    Initial Commit

---
+++ weather-message-api/src/main/java/com/narlock/weathermessageapi/WeatherMessageApiApplication.java
@@ -0,0 +1,13 @@
+package com.narlock.weathermessageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMessageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMessageApiApplication.class, args);
+	}
+
+}
---
+++ weather-message-api/src/main/java/com/narlock/weathermessageapi/controller/WeatherMessageController.java
@@ -0,0 +1,4 @@
+package com.narlock.weathermessageapi.controller;
+
+public class WeatherMessageController {
+}
